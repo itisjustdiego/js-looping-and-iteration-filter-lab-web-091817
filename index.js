26267d144112c768210457389c8eec30c3a6d707
itisjustdiego <itisjustdiego@gmail.com>
2017-10-30 23:22:53 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file
+function findMatching(array, string) {
+  return array.filter(function(name){
+    return name.toLowerCase() === string.toLowerCase()  //Why are we returning twice ???
+  });
+}
+
+function fuzzyMatch(array, string) {
+  return array.filter(function(name){
+    return name.slice(0, 1) === string.slice(0, 1)
+  });
+}
+
+function matchName(array, string) {
+  return array.filter(function(name) {
+    return name.name === string
+  });
+}
